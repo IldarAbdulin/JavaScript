52f31f7f71313281ea39bb53169abf3906b54654
RdQeWaEd <113918653+IldarAbdulin@users.noreply.github.com>
2022-09-24 16:55:47 +0600

    Add files via upload

---
+++ JS 1/for.js
@@ -0,0 +1,26 @@
+//1
+// let a = 30;
+
+// for( i = 0; i<=a; i++ ){
+//     console.log(i++)
+// }
+
+
+//2
+// let a = 30;
+
+// for( i = 0 ; i<=a ; i++ ){
+//         if(i%7==0) {
+//         console.log(i)
+//         }
+//     }
+
+
+//3
+// let a = 1;
+
+// for( i=30; i>=a; i--){
+//     if(i%2==1) {
+//         console.log(i)
+//     }
+// }
---
+++ JS 1/welcome.js
@@ -0,0 +1,54 @@
+//first lesson
+
+
+//1
+// let a = prompt('Введите первое число'),
+//     b = prompt('Введите второе число');
+
+// if(a>b){
+//     alert('>');
+// }else if(a<b){
+//     alert('<');
+// }
+// else{
+//     alert('=')
+// }
+
+
+//2
+// let a = +prompt("Введите число штрафов 1-курсника"),
+//     b = +prompt("Введите число штрафов 2-курсника"),
+//     c = +prompt("Введите число штрафов 3-курсника");
+
+// if (a >= 1) {
+//     alert("1");
+// } else if (b > a) {
+//     alert("1");
+// } 
+// else {
+//     alert("1");
+// }
+
+//3
+
+// let sum = 0;
+// let n = 50;
+
+// for( i = 0 ; i <= n; i++){
+//     console.log(i);
+//     sum+=i
+// }
+
+// console.log(sum)
+
+
+//4
+// let sum = 0;
+// let n = -10;
+
+// for( i = 0 ; i >= n; i--){
+//     console.log(i)
+//     sum+=i
+// }
+
+// console.log(sum)
